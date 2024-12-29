f298265d15172b39aabae0c0f6a64c59d545e85f
Misael Alexandrino <misaelalexandrino02@outlook.com>
2024-12-29 17:43:06 -0300

    Adicionando query 1

--- query1.sql
+++ query1.sql
@@ -1,14 +1,19 @@
-/* "Quero saber quais são os atores que aparecem mais vezes no nosso catálogo de filmes. 
+/*
+1 - Relatório de Atores Populares
+"Quero saber quais são os atores que aparecem mais vezes no nosso catálogo de filmes. 
 Mostre os nomes dos atores e a contagem de filmes em que cada um aparece, 
-ordenados do mais frequente para o menos frequente." */
+ordenados do mais frequente para o menos frequente. 
+Inclua apenas atores que aparecem em mais de 20 filmes."
+*/
 
 SELECT 
-    film_actor.actor_id, 
-    actor.first_name,
-    actor.last_name,
-    COUNT(film_actor.actor_id) AS qtd_filmes 
-FROM sakila.film_actor
-INNER JOIN sakila.actor
-ON actor.actor_id = film_actor.actor_id
-GROUP BY actor_id
-ORDER BY COUNT(film_actor.actor_id) DESC;
+    actor.actor_id AS id_ator,
+    actor.first_name AS nome,
+    actor.last_name AS sobrenome,
+    COUNT(film_actor.actor_id) AS total_filmes
+FROM sakila.actor
+INNER JOIN sakila.film_actor
+ON film_actor.actor_id = actor.actor_id
+GROUP BY actor.actor_id, actor.first_name, actor.last_name
+HAVING COUNT(film_actor.actor_id) > 20
+ORDER BY total_filmes DESC;
--- query5.sql
+++
@@ -1,9 +0,0 @@
-/* "Quanto estamos ganhando, em média, por mês? Faça uma consulta que mostre a 
-receita média por mês de locação, ao longo de todos os anos de dados que temos." */
-
-USE sakila;
-SELECT 
-    DATE_FORMAT(payment.payment_date, "%m/%Y") AS periodo,
-    AVG(payment.amount)  AS total
-FROM payment
-GROUP BY DATE_FORMAT(payment.payment_date, "%m/%Y");
